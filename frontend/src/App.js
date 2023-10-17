32a4c233209daab15b2edf12ffc038698fc1109c
anthonyb0824 <anthonybutler0824@gmail.com>
2023-10-16 23:31:15 -0500

    initialized react app, installed chakar ui and react router dom. Also created homepage and chatpage.

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import { Route } from 'react-router-dom';
+import './App.css';
+import HomePage from "./Pages/Homepage"
+import ChatPage from "./Pages/Chatpage"
+
+function App() {
+  return (
+    <div className="App">Hello
+      <Route path='/' component = {HomePage} exact/>
+      <Route path='/chats' component = {ChatPage}/>
+    </div>
+  );
+}
+
+export default App;
