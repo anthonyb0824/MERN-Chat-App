969ce67b28ab3e3b70cf975b6907833fcdb11b09
anthonyb0824 <anthonybutler0824@gmail.com>
2023-10-22 12:49:43 -0500

    user authentication for login and sing up in the backend

--- backend/server.js
+++ backend/server.js
@@ -2,23 +2,23 @@ const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 const app = express();
 dotenv.config();
 connectDB();
 
+app.use(express.json()); // to accept json data
+
 app.get("/", (req, res) => {
   res.send("API is running for real");
 });
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
+app.use("/api/user", userRoutes);
 
-app.get("/api/chat/:id", (req, res) => {
-  const singleChat = chats.find((c) => c._id === req.params.id);
-  res.send(singleChat);
-});
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server Started on PORT ${PORT}`));
